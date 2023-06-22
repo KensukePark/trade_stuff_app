a22d97b78ea37c7e5f3234b9f44447abf3e36065
HyunWoo <iu4148@gmail.com>
2023-06-22 18:50:51 +0900

    2023-06-22 update

--- lib/screens/modify_page.dart
+++ lib/screens/modify_page.dart
@@ -3,12 +3,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class modifyPage extends StatefulWidget {
-  const modifyPage({Key? key}) : super(key: key);
+  const modifyPage({Key? key, required this.cur_pw}) : super(key: key);
+  final cur_pw;
   @override
   State<modifyPage> createState() => _modifyPageState();
 }
 
 class _modifyPageState extends State<modifyPage> {
+  String new_ps = '';
+  String new_ps_confirm = '';
   void chgPassword(String password) async{
     var user = await FirebaseAuth.instance.currentUser;
     if (user != null) {
@@ -27,12 +30,99 @@ class _modifyPageState extends State<modifyPage> {
         actions: [
           IconButton(
             onPressed: () {
-
+              (new_ps != new_ps_confirm) ? showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    title: Column(
+                      children: [
+                        Text('오류'),
+                      ],
+                    ),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '새 비밀번호와 비밀번호 확인이 일치하지 않습니다.'
+                        ),
+                      ],
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('확인'),
+                      )
+                    ],
+                  );
+                },
+              ): null;
+              //Navigator.pop(context);
             },
             icon: Icon(Icons.check),
           )
         ],
-
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: '새 비밀번호',
+                enabledBorder: InputBorder.none,
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF303030)),
+                ),
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF303030)),
+                ),
+                errorText: new_ps != new_ps_confirm ? 'Password incorrect' : null,
+              ),
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+              onChanged: (value) => new_ps = value,
+              obscureText: true,
+            ),
+            const Divider(thickness: 2,),
+            TextField(
+              decoration: InputDecoration(
+                hintText: '새 비밀번호 확인',enabledBorder: InputBorder.none,
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF303030)),
+                ),
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF303030)),
+                ),
+              ),
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+              onChanged: (value) => new_ps_confirm = value,
+              obscureText: true,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: MediaQuery.of(context).size.height * 0.05,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+                onPressed: (new_ps != new_ps_confirm) ? null : () {print('same');},
+                child: Text('Regist'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
