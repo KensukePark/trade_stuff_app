c6a4d38845c11e27b88ae465def971ba91910cf6
HyunWoo <iu4148@gmail.com>
2023-06-27 18:55:33 +0900

    2023-06-27 update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -35,9 +36,21 @@ class _HomePage extends State<HomePage> {
   late int view_temp;
   late String uid = '';
   int num_item = 0;
+  DateTime? currentBackPressTime;
   Future<void> getUid() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     uid = prefs.getString('uid') ?? '';
+  }
+  Future<bool> onWillPop(){
+    DateTime now = DateTime.now();
+    final msg = "'뒤로가기'버튼을 한 번 더 누르면 종료됩니다.";
+    Fluttertoast.showToast(msg: msg);
+    if(currentBackPressTime == null || now.difference(currentBackPressTime!)
+        > Duration(seconds: 2)) {
+      currentBackPressTime = now;
+      return Future.value(false);
+    }
+    return Future.value(true);
   }
   NumberFormat format = NumberFormat('#,###');
   @override
@@ -83,172 +96,175 @@ class _HomePage extends State<HomePage> {
           });
         },
       ),
-      body: SingleChildScrollView(
-        child: FutureBuilder(
-          future: itemProvider.fetch_Items(),
-          builder: (context, snapshots) {
-            if (time == 0) {
-              DateTime dt = DateTime.now();
-              temp = dt.toString();
-              date_now = temp.substring(0,10);
-              year_now = int.parse(temp.substring(0,4));
-              mon_now = int.parse(temp.substring(5,7));
-              day_now = int.parse(temp.substring(8,10));
-              time_now = temp.substring(11,19); //19까지 초시간
-              hour_now = int.parse(temp.substring(11,13));
-              time = 1;
-            }
-            if (itemProvider.items.isEmpty) {
-              return const Center(child: Text('등록된 물건이 없습니다.'));
-            } else {
-              return ListView.separated(
-                  padding: const EdgeInsets.all(15),
-                  physics: const NeverScrollableScrollPhysics(),
-                  separatorBuilder: (BuildContext context, int index) => const Divider(),
-                  shrinkWrap: true,
-                  itemCount: itemProvider.items.length,
-                  itemBuilder: (context, index) {
-                    view_temp = itemProvider.items[index].view_count;
-                    num_item = itemProvider.items.length;
-                    //int like_temp = itemProvider.items[index].like_count;
-                    if ((itemProvider.items[index].registerDate).substring(0,10) != date_now) {
-                      //ex) 2023-06-04 substring5,7 => 월, 8,10 => 일
-                      var register_year = int.parse((itemProvider.items[index].registerDate).substring(0,4));
-                      var register_mon = int.parse((itemProvider.items[index].registerDate).substring(5,7));
-                      var register_day = int.parse((itemProvider.items[index].registerDate).substring(8,10));
-                      if (register_year == year_now) {
-                        if (register_mon == mon_now) {
-                          register_print = (day_now - register_day).toString() + '일 전';
+      body: WillPopScope(
+        onWillPop: onWillPop,
+        child: SingleChildScrollView(
+          child: FutureBuilder(
+            future: itemProvider.fetch_Items(),
+            builder: (context, snapshots) {
+              if (time == 0) {
+                DateTime dt = DateTime.now();
+                temp = dt.toString();
+                date_now = temp.substring(0,10);
+                year_now = int.parse(temp.substring(0,4));
+                mon_now = int.parse(temp.substring(5,7));
+                day_now = int.parse(temp.substring(8,10));
+                time_now = temp.substring(11,19); //19까지 초시간
+                hour_now = int.parse(temp.substring(11,13));
+                time = 1;
+              }
+              if (itemProvider.items.isEmpty) {
+                return const Center(child: Text('등록된 물건이 없습니다.'));
+              } else {
+                return ListView.separated(
+                    padding: const EdgeInsets.all(15),
+                    physics: const NeverScrollableScrollPhysics(),
+                    separatorBuilder: (BuildContext context, int index) => const Divider(),
+                    shrinkWrap: true,
+                    itemCount: itemProvider.items.length,
+                    itemBuilder: (context, index) {
+                      view_temp = itemProvider.items[index].view_count;
+                      num_item = itemProvider.items.length;
+                      //int like_temp = itemProvider.items[index].like_count;
+                      if ((itemProvider.items[index].registerDate).substring(0,10) != date_now) {
+                        //ex) 2023-06-04 substring5,7 => 월, 8,10 => 일
+                        var register_year = int.parse((itemProvider.items[index].registerDate).substring(0,4));
+                        var register_mon = int.parse((itemProvider.items[index].registerDate).substring(5,7));
+                        var register_day = int.parse((itemProvider.items[index].registerDate).substring(8,10));
+                        if (register_year == year_now) {
+                          if (register_mon == mon_now) {
+                            register_print = (day_now - register_day).toString() + '일 전';
+                          }
+                          else {
+                            register_print = (mon_now - register_mon).toString() + '개월 전';
+                          }
                         }
                         else {
-                          register_print = (mon_now - register_mon).toString() + '개월 전';
+                          register_print = (year_now - register_year).toString() + '년 전';
                         }
                       }
                       else {
-                        register_print = (year_now - register_year).toString() + '년 전';
+                        if (hour_now - int.parse(itemProvider.items[index].registerDate.substring(11,13)) > 0) {
+                          register_print = (hour_now - int.parse(itemProvider.items[index].registerDate.substring(11,13))).toString() + '시간 전';
+                        }
+                        else {
+                          register_print = '방금 전';
+                        }
                       }
-                    }
-                    else {
-                      if (hour_now - int.parse(itemProvider.items[index].registerDate.substring(11,13)) > 0) {
-                        register_print = (hour_now - int.parse(itemProvider.items[index].registerDate.substring(11,13))).toString() + '시간 전';
-                      }
-                      else {
-                        register_print = '방금 전';
-                      }
-                    }
-                    return InkWell(
-                      onTap: () {
-                        final usercol = FirebaseFirestore.instance.collection("items").doc(itemProvider.items[index].id);
-                        usercol.get().then((value) => {
-                          view_temp = (value.data()?['view_count'])
-                        });
-                        if (itemProvider.items[index].user != (widget.email).substring(0, (widget.email).indexOf('@'))) {
-                          view_temp++;
-                          usercol.update({
-                            "view_count": view_temp,
+                      return InkWell(
+                        onTap: () {
+                          final usercol = FirebaseFirestore.instance.collection("items").doc(itemProvider.items[index].id);
+                          usercol.get().then((value) => {
+                            view_temp = (value.data()?['view_count'])
                           });
-                        }
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => ItemDetailPage(
-                                item: itemProvider.items[index],
-                                email: widget.email,)
-                            )
-                        );
-                      },
-                      child: Column(
-                        children: [
-                          SizedBox(height: 5.0,),
-                          Row(
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: Container(
-                                  width: MediaQuery.of(context).size.width * 0.30,
-                                  height: MediaQuery.of(context).size.width * 0.30,
-                                  child: itemProvider.items[index].img == 'null' ?
-                                  Image.asset(
-                                    'images/no_img.jpg', fit: BoxFit.fill
-                                  ) :
-                                  Image.network(
-                                    itemProvider.items[index].img,
-                                    fit: BoxFit.fill,
+                          if (itemProvider.items[index].user != (widget.email).substring(0, (widget.email).indexOf('@'))) {
+                            view_temp++;
+                            usercol.update({
+                              "view_count": view_temp,
+                            });
+                          }
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => ItemDetailPage(
+                                  item: itemProvider.items[index],
+                                  email: widget.email,)
+                              )
+                          );
+                        },
+                        child: Column(
+                          children: [
+                            SizedBox(height: 5.0,),
+                            Row(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width * 0.30,
+                                    height: MediaQuery.of(context).size.width * 0.30,
+                                    child: itemProvider.items[index].img == 'null' ?
+                                    Image.asset(
+                                      'images/no_img.jpg', fit: BoxFit.fill
+                                    ) :
+                                    Image.network(
+                                      itemProvider.items[index].img,
+                                      fit: BoxFit.fill,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              const SizedBox(
-                                width: 15.0,
-                              ),
-                              Container(
-                                height: MediaQuery.of(context).size.width * 0.30,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    SizedBox(height: 5.0,),
-                                    Text(
-                                      itemProvider.items[index].title,
-                                      style: TextStyle(
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                    SizedBox(height: 3.0,),
-                                    Text(
-                                      itemProvider.items[index].loc + ' · ' + register_print,
-                                      style: TextStyle(
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.grey,
-                                      ),
-                                    ),
-                                    SizedBox(height: 3.0,),
-                                    Text(
-                                      format.format(itemProvider.items[index].price) + '원',
-                                      style: TextStyle(
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ],
+                                const SizedBox(
+                                  width: 15.0,
                                 ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Container(height: MediaQuery.of(context).size.width * 0.3 - 24,),
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        Icon(
-                                          Icons.favorite_border,
-                                          color: Colors.grey,
-                                          size: 24,
+                                Container(
+                                  height: MediaQuery.of(context).size.width * 0.30,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      SizedBox(height: 5.0,),
+                                      Text(
+                                        itemProvider.items[index].title,
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
                                         ),
-                                        Text(
-                                          '${itemProvider.items[index].like_count}',
-                                          style: TextStyle(
+                                      ),
+                                      SizedBox(height: 3.0,),
+                                      Text(
+                                        itemProvider.items[index].loc + ' · ' + register_print,
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                      SizedBox(height: 3.0,),
+                                      Text(
+                                        format.format(itemProvider.items[index].price) + '원',
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Container(height: MediaQuery.of(context).size.width * 0.3 - 24,),
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        children: [
+                                          Icon(
+                                            Icons.favorite_border,
                                             color: Colors.grey,
-                                            fontSize: 16,
+                                            size: 24,
+                                          ),
+                                          Text(
+                                            '${itemProvider.items[index].like_count}',
+                                            style: TextStyle(
+                                              color: Colors.grey,
+                                              fontSize: 16,
+                                            )
                                           )
-                                        )
-                                      ],
-                                    ),
-                                  ],
+                                        ],
+                                      ),
+                                    ],
+                                  )
                                 )
-                              )
-                            ],
-                          ),
-                          SizedBox(height: 5.0,),
-                        ],
-                      ),
-                    );
-                  }
-              );
-            }
-          },
+                              ],
+                            ),
+                            SizedBox(height: 5.0,),
+                          ],
+                        ),
+                      );
+                    }
+                );
+              }
+            },
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
