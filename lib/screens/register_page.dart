8bc4aa0cf18f3ce9a98523e78f2a2fadae9dba52
HyunWoo <iu4148@gmail.com>
2023-07-01 01:04:40 +0900

    2023-07-01 update

--- lib/screens/register_page.dart
+++ lib/screens/register_page.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import '../model/auth_model.dart';
 import '../model/register_model.dart';
+import 'home_page.dart';
 
 /* 회원가입 페이지 */
 class RegisterPage extends StatefulWidget {
@@ -17,15 +20,19 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPage extends State<RegisterPage> {
+  bool _idCheck = false;
   bool _authCheck = false;
   bool _sendCheck = false;
   bool _showCheckAuth = false;
+  bool _emailCheck = false;
   late String phoneNum = '';
   late String verifyCode = '';
   late String correctCode = '';
+  late String email = '';
   int _leftTime = 0;
   late Timer _timer;
   FirebaseAuth _auth = FirebaseAuth.instance;
+  late PhoneAuthCredential phoneAuthCredential;
   void signFun(PhoneAuthCredential phoneAuthCredential) async {
     try {
       final authCredential = await _auth.signInWithCredential(phoneAuthCredential);
@@ -36,7 +43,6 @@ class _RegisterPage extends State<RegisterPage> {
           _leftTime = -999;
           _authCheck = true;
         });
-        await _auth.currentUser!.delete();
         _auth.signOut();
       }
     } on FirebaseAuthException catch (e) {
@@ -46,11 +52,79 @@ class _RegisterPage extends State<RegisterPage> {
       });
     }
   }
-
+  void CheckSuccess() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0)
+          ),
+          title: Column(
+            children: <Widget>[
+              new Text("중복확인"),
+            ],
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "사용 가능한 이메일입니다.",
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            new TextButton(
+              child: new Text("확인"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      }
+    );
+  }
+  void CheckFail() {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)
+            ),
+            title: Column(
+              children: <Widget>[
+                new Text("중복확인"),
+              ],
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "사용할 수 없는 이메일입니다.",
+                ),
+              ],
+            ),
+            actions: <Widget>[
+              new TextButton(
+                child: new Text("확인"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        }
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    final authClient =
-    Provider.of<FirebaseAuthProvider>(context, listen: false);
+    final authClient = Provider.of<FirebaseAuthProvider>(context, listen: false);
     return ChangeNotifierProvider(
       create: (_) => RegisterModel(),
       child: Builder(builder: (BuildContext context) {
@@ -61,7 +135,56 @@ class _RegisterPage extends State<RegisterPage> {
               padding: EdgeInsets.all(10),
               child: Column(
                 children: [
-                  EmailInput(),
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width *0.7 - 10,
+                          child: TextField(
+                            onChanged: (value) {
+                              setState(() {
+                                email = value;
+                                _emailCheck = EmailValidator.validate(value);
+                                _idCheck = false;
+                              });
+                            },
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              labelText: 'email',
+                              helperText: '',
+                            ),
+                          ),
+                        ),
+                        //EmailInput(),
+                        //(_sendCheck == false || _showCheckAuth == true) ? null : () {
+                        ElevatedButton(
+                          onPressed: (email == '' || _emailCheck == false) ? null : () async {
+                            int _Checking = 1;
+                            var ps_temp = 'temp123';
+                            try {
+                              UserCredential userCre = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                                email: email, password: ps_temp);
+                            } on FirebaseAuthException catch (e) {
+                              if (e.code == 'email-already-in-use') {
+                                CheckFail();
+                                setState(() {
+                                  _Checking = -1;
+                                });
+                              }
+                            }
+                            if (_Checking == 1) {
+                              FirebaseAuth.instance.currentUser!.delete();
+                              CheckSuccess();
+                              _idCheck = true;
+                            }
+                          },
+                          child: Text('중복확인'),
+                        ),
+                      ],
+                    ),
+                  ),
                   PasswordInput(),
                   PasswordConfirmInput(),
                   Container(
@@ -123,7 +246,6 @@ class _RegisterPage extends State<RegisterPage> {
                                 });
                               },
                             );
-
                           },
                           child: Text('인증요청'),
                         ),
@@ -153,7 +275,7 @@ class _RegisterPage extends State<RegisterPage> {
                         ),
                         ElevatedButton(
                           onPressed: (_sendCheck == false || _showCheckAuth == true) ? null : () {
-                            PhoneAuthCredential phoneAuthCredential =
+                            phoneAuthCredential =
                             PhoneAuthProvider.credential(
                                 verificationId: correctCode, smsCode: verifyCode);
                             signFun(phoneAuthCredential);
@@ -178,7 +300,9 @@ class _RegisterPage extends State<RegisterPage> {
                           borderRadius: BorderRadius.circular(30.0),
                         ),
                       ),
-                      onPressed: (context.read<RegisterModel>().email == '' || //이메일이 입력되지 않았거나
+                      onPressed: (
+                          _idCheck == false || //중복확인을 하지 않았거나 중복된 아이디거나
+                          email == '' || //이메일이 입력되지 않았거나
                           context.read<RegisterModel>().password == '' ||  //패스워드가 입력되지 않았거나
                           context.read<RegisterModel>().password != context.read<RegisterModel>().passwordConfirm || //패스워드 확인이 다르거나
                           _authCheck == false //휴대폰 인증이 되지 않은 경우 회원가입 버튼 비활성화
@@ -192,12 +316,25 @@ class _RegisterPage extends State<RegisterPage> {
                               ..showSnackBar(
                                 SnackBar(content: Text('Regist Success')),
                               );
-                            FirebaseFirestore.instance.collection(context.read<RegisterModel>().email).add({
-                              "key":null//your data which will be added to the collection and collection will be created after this
+                            authClient.auth_login(
+                              context.read<RegisterModel>().email, context.read<RegisterModel>().password
+                            ).then((loginStatus) {
+                              if (loginStatus == AuthStatus.loginSuccess) {
+                                ScaffoldMessenger.of(context)
+                                  ..hideCurrentSnackBar()
+                                  ..showSnackBar(SnackBar(
+                                      content:
+                                      Text('welcome! ' + authClient.user!.email! + ' ')));
+                                _auth.currentUser!.linkWithCredential(phoneAuthCredential);
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(
+                                    email: authClient.user!.email!
+                                )));
+                              } else {
+                                ScaffoldMessenger.of(context)
+                                  ..hideCurrentSnackBar()
+                                  ..showSnackBar(SnackBar(content: Text('login fail')));
+                              }
                             });
-                            //var user_pro = FirebaseAuth.instance.
-                            Navigator.pop(context);
-
                           } else {
                             ScaffoldMessenger.of(context)
                               ..hideCurrentSnackBar()
@@ -225,7 +362,7 @@ class EmailInput extends StatelessWidget {
   Widget build(BuildContext context) {
     final register = Provider.of<RegisterModel>(context, listen: false);
     return Container(
-      padding: EdgeInsets.all(5),
+      width: MediaQuery.of(context).size.width *0.7 - 10,
       child: TextField(
         onChanged: (email) {
           register.setEmail(email);
