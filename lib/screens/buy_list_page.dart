c9bb4d58809396c19e732c8cc09e19f4e6f3a296
HyunWoo <iu4148@gmail.com>
2023-07-01 01:01:11 +0900

    2023-06-30 update

---
+++ lib/screens/buy_list_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BuyPage extends StatefulWidget {
+  const BuyPage({Key? key}) : super(key: key);
+
+  @override
+  State<BuyPage> createState() => _BuyPageState();
+}
+
+class _BuyPageState extends State<BuyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('구매내역'),
+      ),
+    );
+  }
+}
