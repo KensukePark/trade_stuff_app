6da1cf385adecceb79241deb57b99035c1971147
HyunWoo <iu4148@gmail.com>
2023-07-03 04:18:50 +0900

    2023-07-02 update

--- lib/screens/buy_list_page.dart
+++ lib/screens/buy_list_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class BuyPage extends StatefulWidget {
   const BuyPage({Key? key}) : super(key: key);
-
   @override
   State<BuyPage> createState() => _BuyPageState();
 }
