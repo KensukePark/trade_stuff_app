0420615877f3ef905886b8675c3788d3b83544ac
HyunWoo <iu4148@gmail.com>
2023-07-05 04:30:07 +0900

    2023-07-05 update

--- lib/screens/item_detail_page.dart
+++ lib/screens/item_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -19,6 +20,8 @@ enum Menu {
   delete,
 }
 class ItemDetailPageState extends State<ItemDetailPage> {
+  List<String> dropList = [
+    '판매중', '예약중', '판매완료'];
   late String uid = '';
   late int like_temp = widget.item.like_count;
   late int view_temp = widget.item.view_count + 1;
@@ -31,6 +34,7 @@ class ItemDetailPageState extends State<ItemDetailPage> {
   var time_now;
   late int hour_now;
   var register_print;
+  String selectedValue = '판매중';
   Future<void> getUid() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     uid = prefs.getString('uid') ?? '';
@@ -62,7 +66,14 @@ class ItemDetailPageState extends State<ItemDetailPage> {
   Widget build(BuildContext context) {
     final like_provider = Provider.of<LikeProvider>(context);
     final usercol = FirebaseFirestore.instance.collection("items").doc(widget.item.id);
-    if (widget.item.user != (widget.email).substring(0, (widget.email).indexOf('@'))) {
+    setState(() {
+      selectedValue = widget.item.selling;
+    });
+    print(widget.item.idx);
+    print(widget.item.id);
+    print(widget.item.idd);
+
+    if (widget.item.user != widget.email) {
       usercol.update({
         "view_count": view_temp,
       });
@@ -134,7 +145,7 @@ class ItemDetailPageState extends State<ItemDetailPage> {
                           icon: Icon(Icons.arrow_back_ios, color: Colors.white),
                         ),
                       ),
-                      (widget.item.user == (widget.email).substring(0, (widget.email).indexOf('@'))) ?
+                      (widget.item.user == widget.email) ?
                         Positioned(
                           right: 10,
                           top: 10,
@@ -169,7 +180,7 @@ class ItemDetailPageState extends State<ItemDetailPage> {
                               mainAxisAlignment: MainAxisAlignment.start,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(widget.item.user),
+                                Text(widget.item.user.substring(0, widget.item.user.indexOf('@'))),
                                 Text(widget.item.loc)
                               ],
                             )
@@ -186,12 +197,12 @@ class ItemDetailPageState extends State<ItemDetailPage> {
                         ),
                         SizedBox(height: 2,),
                         Text(
-                          widget.item.type,
+                          '- '+ widget.item.type + '·' + register_print,
                           style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.grey),
                         ),
                         SizedBox(height: 2,),
                         Text(
-                          register_print,
+                          '- '+ widget.item.state,
                           style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.grey),
                         ),
                         SizedBox(height: 20.0),
@@ -224,89 +235,147 @@ class ItemDetailPageState extends State<ItemDetailPage> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      format.format(widget.item.price) + '원',
-                      style: TextStyle(fontSize: 16, color: Colors.white),
-                    ),
-                    like_provider.isItem(widget.item) ?
-                    InkWell(
-                      onTap: () {
-                        like_temp--;
-                        final usercol = FirebaseFirestore.instance.collection("items").doc(widget.item.id);
-                        usercol.update({
-                          "like_count": like_temp,
-                        });
-                        like_provider.removeItem(uid, widget.item);
-                      },
-                      child: Column(
-                        children: [
-                          Icon(
-                            Icons.favorite,
-                            color: Colors.blue,
-                          ),
-                        ],
-                      ),
-                    )
-                    :
-                    InkWell(
-                      onTap: () {
-                        if (widget.item.user == (widget.email).substring(0, (widget.email).indexOf('@'))) {
-                          showDialog(context: context, builder: (context) {
-                            return AlertDialog(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
+                    Row(
+                      children: [
+                        like_provider.isItem(widget.item) ?
+                        InkWell(
+                          onTap: () {
+                            like_temp--;
+                            final usercol = FirebaseFirestore.instance.collection("items").doc(widget.item.id);
+                            usercol.update({
+                              "like_count": like_temp,
+                            });
+                            final usercol_3 = FirebaseFirestore.instance.collection(widget.item.idx).where('idd', isEqualTo: widget.item.idd);
+                            usercol_3.limit(1).get().then((value) {
+                              value.docs[0].reference.update({
+                                "like_count": like_temp,
+                              });
+                              print('update complete');
+                            });
+                            like_provider.removeItem(uid, widget.item);
+                          },
+                          child: Column(
+                            children: [
+                              Icon(
+                                Icons.favorite,
+                                color: Colors.blue,
                               ),
-                              title: Text('실패'),
-                              content: Text('자신의 게시물은 관심목록에 추가할 수 없습니다.'),
-                              actions: <Widget>[
-                                TextButton(
-                                  child: new Text("확인"),
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                ),
-                              ],
+                            ],
+                          ),
+                        )
+                            :
+                        InkWell(
+                          onTap: () {
+                            if (widget.item.user == widget.email) {
+                              showDialog(context: context, builder: (context) {
+                                return AlertDialog(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  title: Text('실패'),
+                                  content: Text('자신의 게시물은 관심목록에 추가할 수 없습니다.'),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      child: new Text("확인"),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                  ],
 
-                            );
-                          });
-                        }
-                        else {
-                          like_temp++;
-                          usercol.update({
-                            "like_count": like_temp,
-                          });
-                          like_provider.addItem(uid, widget.item);
-                          final usercol_2 = FirebaseFirestore.instance.collection("likes").doc(uid);
-                          usercol_2.update({
-                            "like_count": like_temp,
-                          });
-                          final usercol_3 = FirebaseFirestore.instance.collection(widget.item.idx).where('id', isEqualTo: widget.item.id);
-                          usercol_3.limit(1).get().then((value) {
-                            value.docs[0].reference.update({
-                              "like_count": like_temp,
-                            });
-                            print('update complete');
-                          });
-                          /*
-                          .limit(1).get().then((QuerySnapshot val) {
-                            val.docs[0].reference.update({
-                              "like_count": like_temp,
-                            });
-                          });
-                           */
-                            print('여기' + '${like_temp}');
-                        }
+                                );
+                              });
+                            }
+                            else {
+                              like_temp++;
+                              usercol.update({
+                                "like_count": like_temp,
+                              });
+                              like_provider.addItem(uid, widget.item);
+                              final usercol_2 = FirebaseFirestore.instance.collection("likes").doc(uid);
+                              usercol_2.update({
+                                "like_count": like_temp,
+                              });
+                              final usercol_3 = FirebaseFirestore.instance.collection(widget.item.idx).where('idd', isEqualTo: widget.item.idd);
+                              usercol_3.limit(1).get().then((value) {
+                                value.docs[0].reference.update({
+                                  "like_count": like_temp,
+                                });
+                                print('update complete');
+                              });
+                            }
+                          },
+                          child: Column(
+                            children: [
+                              Icon(
+                                Icons.favorite_border,
+                                color: Colors.blue,
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                          child: VerticalDivider(
+                            thickness: 1,
+                            width: 20,
+                          ),
+                        ),
+                        Text(
+                          format.format(widget.item.price) + '원',
+                          style: TextStyle(fontSize: 17, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    (widget.item.user != widget.email) ?
+                    InkWell(
+                      onTap: () {
+
                       },
-                      child: Column(
-                        children: [
-                          Icon(
-                            Icons.favorite_border,
+                      child: Container(
+                        width: 80,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          border: Border.all(
                             color: Colors.blue,
                           ),
-                        ],
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          color: Colors.blue
+                        ),
+                        child: Text(
+                          '구매하기',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
                     )
-
+                    : DropdownButton(
+                        value: selectedValue,
+                        items: dropList.map((String item) {
+                          return DropdownMenuItem<String>(
+                            child: Text('$item'),
+                            value: item,
+                          );
+                        }).toList(),
+                        onChanged: (dynamic value) {
+                          setState(() {
+                            selectedValue = value;
+                          });
+                          usercol.update({
+                            "selling": value,
+                          });
+                          final usercol_3 = FirebaseFirestore.instance.collection(widget.item.idx).where('idd', isEqualTo: widget.item.idd);
+                          usercol_3.limit(1).get().then((val) {
+                            val.docs[0].reference.update({
+                              "selling": value.toString(),
+                            });
+                            print('update complete2');
+                          });
+                        }
+                    ),
                   ],
                 ),
               ],
